63cc418b90402d2d24b07c48088f2ce6554dfa09
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:48:42 +0700

    Initial commit

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+const perulangan = function () {
+    var kata = "Semangat..."
+    var bintang = "*"
+    console.log(kata.repeat(5))
+
+    console.log(bintang.repeat(5))
+    console.log(bintang.repeat(20))
+    console.log(bintang.repeat(30))
+    console.log(bintang.repeat(50))
+    console.log(bintang.repeat(80))
+    console.log(bintang.repeat(50))
+    console.log(bintang.repeat(30))
+    console.log(bintang.repeat(20))
+    console.log(bintang.repeat(5))
+}
+perulangan()
